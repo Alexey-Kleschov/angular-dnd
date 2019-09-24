3562c36930019b45ba2d73bf3ce72b6e70e90b9e
Alexey-Kleschov <iron-goga@yandex.ru>
2019-09-24 15:17:37 +0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragEnter, CdkDragExit, CdkDragStart, CdkDrag } from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent{
+  dropped(event: CdkDragDrop<string[]>) {
+    console.log('dropped');
+  }
+
+  entered(event: CdkDragEnter<string[]>) {
+    console.log('entered');
+  }
+
+  exited(event: CdkDragExit<string[]>) {
+    console.log('exited');
+  }
 }
