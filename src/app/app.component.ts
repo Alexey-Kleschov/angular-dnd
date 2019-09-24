f91eeef5fe66a3b06f9efb06d90949650ce7b1b3
Alexey-Kleschov <iron-goga@yandex.ru>
2019-09-24 16:01:37 +0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragEnter, CdkDragExit, CdkDragStart, CdkDrag } from '@angular/cdk/drag-drop';
-
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragEnter, CdkDragExit, CdkDragStart, CdkDrag, CdkDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'my-app',
@@ -8,6 +7,12 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragEnter, CdkDragE
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent{
+
+newItems = [
+    'Item 0',
+    'Item 1'  
+  ]
+
   dropped(event: CdkDragDrop<string[]>) {
     console.log('dropped');
   }
